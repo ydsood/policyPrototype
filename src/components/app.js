cc1019aaab6badc85a4cc49c50a90da3bad0ca9a
Dan Kirlin <dankirlin@gmail.com>
2017-09-07 16:31:29 -0400

    We have connection to our API!

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        React simple starter
         <SearchBar />
       </div>
     );
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,18 +1,26 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
 import { fetchPolicies } from '../actions';
+import { Button, FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class SearchBar extends Component {
-    testing() {
-        console.log(this.props);
-        this.props.fetchPolicies();
-    }
-
     render() {
-        console.log(this.props.fetchPolicies);
         return(
-            <form onSubmit={ () => console.log('foo') }>
-                <input type='text' onChange={ this.testing.bind(this) }></input>
+            <form
+                className='searchbar' 
+                onSubmit={ this.props.fetchPolicies() }>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type='text' placeholder='search'/>
+                        <InputGroup.Button>
+                            <Button 
+                                type='submit'
+                                bsStyle='primary'>
+                                Search
+                            </Button>
+                        </InputGroup.Button>                      
+                    </InputGroup>
+                </FormGroup>
             </form>
         );
     }
