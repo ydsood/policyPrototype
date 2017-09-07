3a2aeacc2e33fdc0ca4c9abfcaae96a49c9db2d4
Dan Kirlin <dankirlin@gmail.com>
2017-09-07 18:26:07 -0400

    Fixing api request

--- src/reducers/reducer_policies.js
+++ src/reducers/reducer_policies.js
@@ -3,7 +3,7 @@ import FETCH_POLICIES from '../actions';
 export default function(state = [], action) {
     switch (action.type) {
       case FETCH_POLICIES:
-        return action.payload;
+        return action.payload.data;
       default:
         return state
     }
