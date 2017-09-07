3d032ea32cc49d795e7d0f11cdf4e917672d1c08
Yi Li <yli@fasttechnology.com>
2017-09-07 17:20:35 -0400

    new way to display middle

--- src/components/policy/policy_middle_panel.js
+++ src/components/policy/policy_middle_panel.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PolicyTabDetailPanel from './policy_tab_detail_panel';
 import PolicyTabPanel from './policy_tab_panel';
-
+import {Tab, Tabs} from 'react-bootstrap';
 
 const PolicyMiddlePanel = (props) =>{
-    const tabs = props.data.tabs;
+    const tabsData = props.data.tabs;
     const tabItems = props.data.items;
+    console.log(tabsData);
+    const tabs = tabsData.map(tab=>{
+        return (
+            <Tab key={tab.name} eventKey={tab.name} title={tab.name}>
+                <PolicyTabDetailPanel name={tab.name} items={tabItems}/>
+            </Tab>
+        )
+    });
+    console.log(tabs);
     return(
-        <div>
-            <PolicyTabPanel tabs={tabs}/>
-            <PolicyTabDetailPanel items={tabItems}/>
-        </div>
-    )
+        <Tabs defaultActiveKey={tabs[0].name} animation={false} id="noanim-tab">
+            {tabs}
+        </Tabs>
+    );
 }
 
 export default PolicyMiddlePanel;
-
