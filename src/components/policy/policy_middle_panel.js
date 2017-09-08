02fc5f009911cce69174c821e79e9dafca585dc1
Dan Kirlin <dankirlin@gmail.com>
2017-09-08 16:47:43 -0400

    Adding dropdown tab

--- src/components/policy/policy_middle_panel.js
+++ src/components/policy/policy_middle_panel.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import PolicyTabDetailPanel from './policy_tab_detail_panel';
-import PolicyTabPanel from './policy_tab_panel';
-import {Tab, Tabs} from 'react-bootstrap';
+import {Tab, Tabs, NavDropdown, Row, Col, Nav, NavItem, MenuItem} from 'react-bootstrap';
 
 const PolicyMiddlePanel = (props) =>{
     const tabsData = props.data.tabs;
     const tabItems = props.data.items;
-    const tabs = tabsData.map(tab=>{
+    const firstTabsLength = Math.min(3, tabsData.length)
+    const firstTabs = tabsData.slice(0, firstTabsLength).map(item => {
         return (
-            <Tab key={tab.name} eventKey={tab.name} title={tab.name}>
-                <PolicyTabDetailPanel name={tab.name} items={tabItems}/>
-            </Tab>
-        )
+            <NavItem eventKey={ item.name }>
+                {item.name}
+            </NavItem>
+        );
     });
+
+    const dropdownItems = tabsData.slice(firstTabsLength).map(tab => {
+        return <MenuItem eventKey={ tab.name }>{ tab.name }</MenuItem>;
+    });
+
+    const dropDownTab = <NavDropdown title='Additional Options'>{ dropdownItems }</NavDropdown>;
+    const tabContents = tabsData.map(tab => {
+        return (
+            <Tab.Pane eventKey={ tab.name }>
+                <PolicyTabDetailPanel name={ tab.name } items= {tabItems} />
+            </Tab.Pane>
+        );
+    })
+
     return(
-        <Tabs defaultActiveKey={tabs[0].name} animation={false} id="noanim-tab">
-            {tabs}
-        </Tabs>
+        <Tab.Container defaultActiveKey= {tabsData[0].name} id='noanim-tab'>
+            <Row className='clearfix'>
+                <Col sm={12}>
+                    <Nav bsStyle='tabs'>
+                        {firstTabs}
+                        {dropDownTab}
+                    </Nav>
+                </Col>
+                <Col sm={12}>
+                    <Tab.Content animation>
+                        {tabContents}
+                    </Tab.Content>
+                </Col>
+            </Row>
+        </Tab.Container>
     );
 }
 
