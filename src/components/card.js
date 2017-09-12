48861e65eeaa0ee222f74491f85f6c09cfd23a25
Yi Li <yli@fasttechnology.com>
2017-09-12 11:53:01 -0400

    modify ul style and turncate date string

--- src/components/card.js
+++ src/components/card.js
@@ -26,15 +26,20 @@ class Card extends Component {
       (item) => { return !item.name.includes("ID")&&!item.name.includes("Code")}
     )
     .map((item) => {
+      let value = item.value;
+      if(typeof value === 'string' && Date.parse(value)){
+        console.log(item.value);
+        value = item.value.split('T')[0];
+      }
       return (
-        <CardItem key={item.name} name={item.name} value={item.value}/>
+        <CardItem key={item.name} name={item.name} value={value}/>
       );
     });
 
     return (
       <div className = "card" onClick={ this.handleSelect.bind(this)}>
         <Thumbnail>
-          <h4>{this.props.title}</h4>
+          <h4>{this.props.title}</h4>          
           <ul className="unorderdList">
             { valueList }
           </ul>
